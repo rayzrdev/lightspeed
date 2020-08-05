93775a200675771daa9ac86f5bbc0790921bf429
Rayzr522 <rayzr522@gmail.com>
2020-08-05 16:28:31 -0400

    Ignore more files, nogui start.sh

--- start.sh
+++ start.sh
@@ -15,4 +15,4 @@ if [[ ! -f "$JARFILE" ]]; then
     fail "Make sure to copy tuinity-paperclip.jar into this folder before running the server."
 fi
 
-java -Xms"$INIT_MEM" -Xmx"$MAX_MEM" -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar "$JARFILE" -W worlds
+java -Xms"$INIT_MEM" -Xmx"$MAX_MEM" -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar "$JARFILE" -W worlds nogui
