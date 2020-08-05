8fc6d77b126eb91f38e6e12be5bdb3340ae7617f
Rayzr522 <rayzr522@gmail.com>
2020-08-05 16:22:03 -0400

    Update start.sh file to account for missing JAR

--- start.sh
+++ start.sh
@@ -4,6 +4,15 @@ JARFILE=tuinity-paperclip.jar
 MAX_MEM=3G
 INIT_MEM=2G
 
-cd "$(dirname "$0")" || exit 1
+fail() {
+    echo "$*" >&1
+    exit 1
+}
+
+cd "$(dirname "$(realpath "$0")")" || exit 1
+
+if [[ ! -f "$JARFILE" ]]; then
+    fail "Make sure to copy tuinity-paperclip.jar into this folder before running the server."
+fi
 
 java -Xms"$INIT_MEM" -Xmx"$MAX_MEM" -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar "$JARFILE" -W worlds
